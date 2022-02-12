ec48c91b2862f740107934d307b8722b1f7de034
marcelmukundi <marcel.mukundi@wsu.edu>
2022-02-11 20:55:13 -0800

    Small parity/naming changes for consistency

--- ER_v1.sql
+++ ER_v1.sql
@@ -1,85 +1,109 @@
-CREATE TABLE User (
+CREATE TABLE Users
+(
 	user_id VARCHAR PRIMARY KEY,
-	name VARCHAR,
-	average_stars DOUBLE,
-	fan_count INTEGER,
+	first_name VARCHAR,
 	yelping_since DATETIME,
-	vote_count INTEGER,
 	tip_count INTEGER,
-	tip_like_count INTEGER,
+	fan_count INTEGER,
+	average_stars FLOAT,
+
+	funny INTEGER,
+	useful INTEGER,
+	cool INTEGER,
+
 	longitude FLOAT,
 	latitude FLOAT
 );
 
-CREATE TABLE Friend (
-	friend_user_id VARCHAR PRIMARY KEY
-);
 
-CREATE TABLE UserHasFriend (
-	friend_user_id VARCHAR,
+CREATE TABLE FriendedUsers
+(
 	user_id VARCHAR,
-	PRIMARY KEY (friend_user_id, user_id),
-	FOREIGN KEY(friend_user_id) REFERENCES Friend(friend_user_id),
-	FOREIGN KEY (user_id) REFERENCES User(user_id)
+	friended_by_id VARCHAR,
+	PRIMARY KEY (friended_by_id, user_id),
+	FOREIGN KEY(friended_by_id) REFERENCES Users(friended_by_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Business (
+CREATE TABLE Businesses
+(
 	business_id VARCHAR PRIMARY KEY,
-	business_rating FLOAT,
+	name VARCHAR,
 	address VARCHAR,
 	state VARCHAR,
 	city VARCHAR,
 	zip INTEGER,
+
 	longitude FLOAT,
 	latitude FLOAT,
-	business_tip_count INTEGER,
-	business_name VARCHAR,
-	business_checkin_count INTEGER
-	attributes VARCHAR,
+	stars FLOAT,
+
 	is_open INTEGER,
+
+	tip_count INTEGER,
+	checkin_count INTEGER,
 );
 
-CREATE TABLE Categories (
+CREATE TABLE Category
+(
 	business_id VARCHAR,
 	category VARCHAR,
 	PRIMARY KEY (business_id, category),
 	FOREIGN KEY (business_id) REFERENCES Business(business_id)
 );
-	
 
-CREATE TABLE Tip (
+
+CREATE TABLE Tips
+(
 	user_id VARCHAR,
 	business_id VARCHAR,
-	timestamp DATETIME,
-	text VARCHAR,
+	date_posted DATETIME,
+	body VARCHAR,
 	likes INTEGER,
-	PRIMARY KEY (user_id, business_id, timestamp) 
-	FOREIGN KEY (user_id) REFERENCES User(user_id),
+	PRIMARY KEY (user_id, business_id, date_posted),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
 	FOREIGN KEY (business_id) REFERENCES Business(business_id)
 );
 
-CREATE TABLE Check_Ins (
-	business_id,
-	dateTime DATETIME,
-	PRIMARY KEY (business_id),
+CREATE TABLE CheckIns
+(
+	business_id VARCHAR,
+	checkin_date DATETIME,
+	PRIMARY KEY (business_id, checkin_date),
 	FOREIGN KEY (business_id) REFERENCES Business(business_id)
 );
 
-CREATE TABLE Hours (
-	business_id PRIMARY KEY,
-	monday_hours,
-	tuesday_hours,
-	wednesday_hours,
-	thursday_hours,
-	friday_hours,
-	saturday_hours,
-	sunday_hours,
+CREATE TABLE OperatingHours
+(
+	business_id VARCHAR PRIMARY KEY,
+	monday_open TIME,
+	monday_close TIME,
+
+	tuesday_open TIME,
+	tuesday_close TIME,
+
+	wednesday_open TIME,
+	wednesday_close TIME,
+
+	thursday_open TIME,
+	thursday_close TIME,
+
+	friday_open TIME,
+	friday_close TIME,
+
+	saturday_open TIME,
+	saturday_close TIME,
+
+	sunday_open TIME,
+	sunday_close TIME,
 	FOREIGN KEY (business_id) REFERENCES Business(business_id)
 );
 
-CREATE TABLE Attributes (
+CREATE TABLE Attributes
+(
 	business_id VARCHAR,
 	attribute VARCHAR,
+	value VARCHAR,
 	PRIMARY KEY (business_id, attribute),
 	FOREIGN KEY (business_id) REFERENCES Business(business_id)
 );
